9c164450a2522446384ce8a2a039a386be5ac7db
bravint <bravint@gmail.com>
2022-02-13 15:32:37 +0000

    index.jsx: change to HashRouter for gh-pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './styles/reset.css';
 
 import { App } from './App';
 
 ReactDOM.render(
-    <BrowserRouter>
+    <HashRouter>
         <React.StrictMode>
             <App />
         </React.StrictMode>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
