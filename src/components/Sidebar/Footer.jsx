0835175f57929da27d384f125962bd1a64a53c3f
bravint <bravint@gmail.com>
2023-06-18 06:42:00 +0100

    fix footer links

--- src/components/Sidebar/Footer.jsx
+++ src/components/Sidebar/Footer.jsx
@@ -7,11 +7,11 @@ export const Footer = () => {
                 <a href="https://github.com/bravint" target="_blank" rel="noreferrer">
                     <img src={process.env.PUBLIC_URL + `/assets/images/github-icon.svg`} alt="gitHub logo" className={styles.footerLogo} />
                 </a>
-                <a href="https://uk.linkedin.com/" target="_blank" rel="noreferrer">
+                <a href="https://www.linkedin.com/in/bravint/" target="_blank" rel="noreferrer">
                     <img src={process.env.PUBLIC_URL + `/assets/images/linkedin-icon.svg`} alt="LinkedIn Logo" className={styles.footerLogo} />
                 </a>
             </section>
-            <p>&#169;2022 Bravin Thillainathan</p>
+            <p>&#169;2023 Bravin Thillainathan</p>
         </>
     );
 };
