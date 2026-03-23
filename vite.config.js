4a8df311e724a5f4acc7acfd08d7f89cc8040298
Bravin Thillainathan <89780339+bravint@users.noreply.github.com>
2026-03-23 15:48:02 +0000

    fix: fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/',
+    base: '/football-stats/',
     plugins: [react()],
-    server: {    
+    server: {
         open: true,
-        port: 3000, 
+        port: 3000,
     },
-})
+});
