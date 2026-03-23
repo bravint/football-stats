ae61795b250b573d808f38eb493da9cb4466e7f6
Bravin Thillainathan <89780339+bravint@users.noreply.github.com>
2026-03-23 17:12:43 +0000

    build: deploy to dokploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '',
+    base: '/football-stats/',
     plugins: [react()],
     server: {
         open: true,
